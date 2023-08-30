8f1392cf79b0b915d043e348c917ba00668f7cf2
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-08-30 21:14:10 +0300

    create Booking, Menu models

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
+        'CONN_MAX_AGE': 5,
     }
 }
 
